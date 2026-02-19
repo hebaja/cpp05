92a1b3e8b23fe2845942bb6bffa5875124630dde
Henrique <hebaja@yahoo.com.br>
2026-02-19 19:23:46 -0300

    minor changes in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,12 +7,8 @@ int	main()
 		Bureaucrat a("Warrel", 32);
 		Form f("Nevermore", 46, 35);
 		std::cout << a << " - " << f << std::endl;
-		try {
-			a.signForm(f);
-			std::cout << f << std::endl;
-		} catch (std::exception &e) {
-			std::cout << "Error: " << e.what() << std::endl;
-		}
+		a.signForm(f);
+		std::cout << f << std::endl;
 	} catch (std::exception &e) {
 		std::cout << "Error: " << e.what() << std::endl;
 	}
