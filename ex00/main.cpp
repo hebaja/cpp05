7682e00a51e922cb2926992a7ec5b95124457cfb
hebatist <hebaja@yahoo.com.br>
2026-02-23 20:08:09 -0300

    ex00 more tests added to main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,6 +2,29 @@
 
 int	main()
 {
+	// DEFAULT TESTING 
+	{
+		Bureaucrat a("John", 121);
+		Bureaucrat b("Ronnie", 5);
+		Bureaucrat c(a);
+		Bureaucrat d("Kirk", 66);
+
+		std::cout << "a -> " << a << std::endl;
+		std::cout << "b -> "<< b << std::endl;
+		b = a;
+		std::cout << "b -> " << b << std::endl;
+		std::cout << "c -> "<< c << std::endl;
+
+		a.incrementGrade();
+		std::cout << "a -> " << a << std::endl;
+		a.decrementGrade();
+		std::cout << "a -> " << a << std::endl;
+
+		std::cout << "d -> " << d << std::endl;
+		d = b;
+		std::cout << "d -> " << d << std::endl;
+	}
+	/*
 	{
 		try {
 			Bureaucrat a("Raphael", 15);
@@ -24,12 +47,14 @@ int	main()
 			std::cout << "Could not instantiate class. " << e.what() << std::endl;
 		}
 	}
+	*/
 
 	/*
 	{
 		// INCREMENT DECREMENT EXCEPTIONS
 		Bureaucrat a("Archimedes", 1);
 		Bureaucrat b("Tesla", 150);
+		Bureaucrat c("Einstein", 25);
 		
 		std::cout << a << std::endl;
 		try {
@@ -47,28 +72,16 @@ int	main()
 			std::cout << "Could not decrement grade. " << e.what() << std::endl;
 		}
 		std::cout << b << std::endl;
+
+		for (int i = 0; i < 5; i++) {
+			c.incrementGrade();
+			std::cout << c << std::endl;
+		}
+		for (int i = 0; i < 5; i++) {
+			c.decrementGrade();
+			std::cout << c << std::endl;
+		}
 	}
 	*/
-	
-	// DEFAULT TESTING 
-	/*
-	{
-		Bureaucrat a("John", 121);
-		Bureaucrat b("Ronnie", 5);
-		Bureaucrat c(a);
-
-		std::cout << a << std::endl;
-		std::cout << b << std::endl;
-		b = a;
-		std::cout << b << std::endl;
-		std::cout << c << std::endl;
-
-		a.incrementGrade();
-		std::cout << a << std::endl;
-		a.decrementGrade();
-		std::cout << a << std::endl;
-	}
-	*/
-
 	return (0);
 }
